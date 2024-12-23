9087b5ef9ad9c5af95845e4f0f345025e4a2c73b
RickNunes <henriquenunes0510@icloud.com>
2024-12-23 15:45:27 -0300

    Mostrando resultado do IMC na proxima tela

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,12 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Recuparar os componentes EditText
-        // Criar uma variavel e associar o componente de UI <EditText>
-        // Recuperar o botao da tela
 
-        //Colocar acao no botao setOnClickListener
-        // Recuperar o texto digitado no edt peso.
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
@@ -31,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             val alturaStr: String = edtAltura.text.toString()
 
           if (pesoStr == "" || alturaStr  == "") {
-              // Mostrar mensagem para o usuar
+
 
               Snackbar
                   .make(
@@ -47,6 +43,18 @@ class MainActivity : AppCompatActivity() {
 
               val alturaQ2 = altura * altura
               val resultado = peso / alturaQ2
+
+              // Navegar para a proxima tela
+              // Criar o layout da proxima tela
+              // Passar dados (resultado) para proxima tela
+
+              // Intent - Classe do proprio android
+
+              val intent = Intent(this, ResultActivity::class.java)
+              intent.putExtra(KEY_RESULT_IMC, resultado)
+
+              startActivity(intent)
+
               println("Roque acao do botao" + resultado)
           }
 
